7da2352f3541bd38d041c397f697df9e1f759cd5
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2020-10-08 13:16:01 +0600

    forms with get method

---
+++ html forms/index.php
@@ -0,0 +1,22 @@
+<h1>HTML forms</h1>
+
+<form>
+
+  <p>
+    <label for="guess">Input Guess</label>
+    <input type="text" name="guess" id="guess">
+    <input type="submit">
+
+  </p>
+
+</form>
+
+
+
+
+<pre>
+$_GET
+<?php
+  print_r($_GET);
+ ?>
+</pre>
