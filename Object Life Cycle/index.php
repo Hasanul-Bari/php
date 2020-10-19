b4e2a0bd16357e8e6a118a18a9d1200476342caf
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2020-10-19 22:10:33 +0600

    constructor

---
+++ Object Life Cycle/index.php
@@ -0,0 +1,33 @@
+<h1>Object Life Cycle</h1>
+
+life cycle of construtor-destructor<br><br>
+<?php
+
+  class PartyAnimal
+  {
+      function __construct() {
+      echo("Constructed<br>");
+      }
+
+      function something() {
+      echo("Something<br>");
+      }
+
+      function __destruct() {
+      echo("Destructed<br>");
+      }
+  }
+
+
+  echo("--One<br>");
+
+  $x = new PartyAnimal();
+  echo("--Two<br>");
+
+  $y = new PartyAnimal();
+  echo("--Three<br>");
+
+  $x->something();
+  echo("--The End?<br>");
+
+ ?>
